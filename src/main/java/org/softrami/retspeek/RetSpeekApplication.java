560ab29b03b7ace91c807369b163605defadc0ac
Ramiro Nuñez <raminp.1012@gmail>
2024-10-03 12:59:54 -0500

    Proyecto base

---
+++ src/main/java/org/softrami/retspeek/RetSpeekApplication.java
@@ -0,0 +1,13 @@
+package org.softrami.retspeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetSpeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetSpeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/softrami/retspeek/RetSpeekApplicationTests.java
@@ -0,0 +1,13 @@
+package org.softrami.retspeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetSpeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
